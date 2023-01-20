88f14a1e1e1285d6f11995cc9099caae4d27b839
MarcosSouza <marcossouza.git@gmail.com>
2023-01-20 13:18:22 -0300

    foi adicionado bootstrap no projeto e limpei as variáveis, css e ejs antigos.

--- index.js
+++ index.js
@@ -8,33 +8,11 @@ app.set('view engine', 'ejs');
 //Vou criar acesso para arquivos estáticos
 app.use(express.static('public'));
 
-//criação da rota com dois parâmetros 
-app.get("/:nome/:lang", (req, res) => {
-
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    //utilizando forEach
-    var produtos = [
-        { nome: "Doritos", preco: 3.14 },
-        { nome: "Coca-Cola", preco: 10.00 },
-        { nome: "Leite", preco: 6.25 },
-        { nome: "Macarrão", preco: 7.25 },
-        { nome: "Feijão", preco: 10.32 },
-        { nome: "Arroz", preco: 7.20 },
-        { nome: "Farinha", preco: 3.38 }
-    ]
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "MS Produções",
-        funcionarios: 60,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+//criação da rota 
+app.get("/", (req, res) => {
+    res.render("index")
 });
+
 
 // Start da aplicação
 app.listen(8080, (erro) => {
